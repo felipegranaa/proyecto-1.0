4a41b1fbb5a71744a0e5bf04c43f8e79e47bdd01
felipegranaa <156699776+felipegranaa@users.noreply.github.com>
2024-01-15 14:26:06 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+meme_dickt ={"lol": "risa",
+            "cringe": "algo extremadamente raro o verguensa agena",
+            "xd": "risa",
+            "creepy": "aterrador",
+            "emo": "alguien emo es una persona depresiva\o"}
+            
+
+for i in range(4):
+    palabra = input("hola que palabra modernas queres saber su significado")
+    print(meme_dickt[palabra])
